86bef1f785aa1f61a5742143ce930c817e80628d
KamoteCoders <jowelpana44@gmail.com>
2023-11-13 14:17:42 +0800

    jowel

--- src/app/gantchart/gantchart.component.ts
+++ src/app/gantchart/gantchart.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-gantchart',
   templateUrl: './gantchart.component.html',
-  styleUrls: ['./gantchart.component.css']
+  styleUrls: ['./gantchart.component.css'],
 })
-export class GantchartComponent {
-
-}
+export class GantchartComponent {}
