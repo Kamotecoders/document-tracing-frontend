784b0e34f761bf5bab36c7c55923ea29737f7b2a
ketchupzzz <jmballangca002@gmail.com>
2023-12-13 02:56:35 +0800

    firebase version

--- src/app/views/admin/admin-home/admin-home.component.ts
+++ src/app/views/admin/admin-home/admin-home.component.ts
@@ -90,4 +90,86 @@ export class AdminHomeComponent {
       .then((data) => this.toastr.success(`Appointment ${status}`))
       .catch((err) => this.toastr.error(err));
   }
+
+  filterAppointmentByType(purpose: number) {
+    this._scheduledAppointments = this._allAppointment.filter(
+      (e) => e.purpose == this.getPurpose(purpose)
+    );
+  }
+  getPurpose(num: number) {
+    switch (num) {
+      case 1:
+        return "VOTER'S CERTIFICATE";
+
+      case 2:
+        return 'PSA';
+
+      case 3:
+        return 'KASALANG BAYAN';
+
+      case 4:
+        return 'BURIAL ASSISTANCE';
+
+      default:
+        return;
+        "VOTER'S CERTIFICATE";
+    }
+  }
+
+  printPage() {
+    const documentToPrint = document.getElementById('appointment');
+    const logo = document.getElementById('logo');
+    if (documentToPrint) {
+      const popupWin = window.open('', '_blank', 'width=800,height=600');
+      if (popupWin) {
+        popupWin.document.open();
+        popupWin.document.write(`
+        <html>
+          <head>
+            <title>Print</title>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+            <style>
+              /* Add your custom styles for printing here */
+              @media print {
+                body {
+                  margin: 0;
+                  padding: 20px; /* Add padding for better formatting */
+                }
+                .centered-image {
+                  display: block;
+                  margin: 0 auto; /* Set margins to auto to center the image */
+                }
+              }
+            </style>
+          </head>
+          <body onload="window.print();window.onafterprint=function(){window.close();}">
+            <div style="text-align: center;"> <!-- Center content -->
+              <img src="../../../assets/images/logo.png" class="centered-image" width="100" height="100" />
+              <h5>Appointment Report</h5>
+              ${documentToPrint.innerHTML}
+            </div>
+          </body>
+        </html>
+      `);
+        popupWin.document.close();
+      } else {
+        console.error('Failed to open popup window for printing.');
+      }
+    } else {
+      console.error(`Document with ID 'appointment' not found.`);
+    }
+  }
+  statusChecker(status: string) {
+    if (status === 'schedulled') {
+      return 'scheduled';
+    }
+    return status;
+  }
+
+  onSearch(e: any) {
+    const inputValue: string = e.target.value.toLocaleLowerCase();
+    this._scheduledAppointments = this._allAppointment.filter((data) => {
+      return data.fullname.toLocaleLowerCase().includes(inputValue);
+    });
+  }
 }
